d558e528001c03e20b4ec99a0b1cb183740d198c
Emily Xiong <xiongemi@gmail.com>
2021-04-16 21:47:16 -0400

    add firebase deploy

--- apps/forum/gatsby-ssr.js
+++ apps/forum/gatsby-ssr.js
@@ -5,7 +5,6 @@
  */
 
 // You can delete this file if you're not using it
-import 'firebase/auth';
 import 'firebase/firestore';
 
 import WrapWithProvider from './src/components/wrap-with-provider';
